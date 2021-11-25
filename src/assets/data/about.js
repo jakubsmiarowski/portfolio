5f006e19c08fccc93c0b52bed842dbe6067b093e
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2021-11-25 15:48:21 -0500

    removed unnecessary photos, made changes to list

---
+++ src/assets/data/about.js
@@ -0,0 +1,46 @@
+const about = [
+    { id: 1, text: "1. I'm 29", em4oji: null },
+    { id: 2, text: "2. Love rock climbing 🧗", emoji: null },
+    { id: 3, text: "3. Main transportation - bike", emoji: "\u{1F6B4}" },
+    {
+        id: 4,
+        text: "4. Coolest place I've been? Australian Rainforest",
+        emoji: "\u{1F334}",
+    },
+    {
+        id: 5,
+        text: "5. Reeealy into coding, getting better by the day",
+        emoji: "\u{1F468}",
+    },
+    {
+        id: 6,
+        text: "6. Like making photos, not being on them",
+        emoji: "\u{1F4F8}",
+    },
+    {
+        id: 7,
+        text:
+            "7. Organized a trip for my 16 friends to go to Thailand. Weirdest trip ever",
+        emoji: "\u{1F30F}",
+    },
+    { id: 8, text: "8. 'Breaking Bad' all the way!", emoji: "\u{1F4FA}" },
+    { id: 9, text: "9. King of board games", emoji: "\u{1F3B2}" },
+    {
+        id: 10,
+        text: "10. Favorite quote: 'I find your lack of faith... disturbing'",
+        emoji: null,
+    },
+    {
+        id: 11,
+        text: "11. Tequila doesn't like me as much as I like Tequila",
+        emoji: null,
+    },
+    {
+        id: 12,
+        text: "12. My dream job? Blockchain Developer",
+        emoji: "🔥",
+    },
+    { id: 13, text: "13. I hope to meet you", emoji: "\u{1F44B}" },
+    { id: 14, text: "14. And have a fantastic day!", emoji: "\u{1F609}" },
+];
+export default about;
--- src/features/example.feature
+++
@@ -1,6 +0,0 @@
-Feature #FeatureLine First visit
-  Scenario: Display "main" page
-    Given I visit "main" page
-    And Title is "Kuba Śmiarowski"
-    Then I click the "contact" button
-    And Contact form is visible
